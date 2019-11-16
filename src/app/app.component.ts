2da2059ee3c5cf4138726812b027e22cc9bee0d9
Michel Vollebregt <mi.vollebregt@gmail.com>
2019-11-16 15:12:24 +0100

    prefix, formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'mar-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
